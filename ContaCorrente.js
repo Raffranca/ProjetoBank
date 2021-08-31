e5677c9714d32e1ff107ff32ffe513caa001cf65
Rafaela França <rafaelafrancamedeiros@gmail.com>
2021-08-31 01:13:29 -0300

    projetobank

---
+++ ContaCorrente.js
@@ -0,0 +1,41 @@
+import { Cliente } from "./Cliente";
+
+export class ContaCorrente {
+    agencia;
+    _cliente;
+
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
+    _saldo = 0;
+
+    get saldo(){
+        return this._saldo;
+    }
+
+    sacar(valor) {
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor <= 0){
+            return;
+        }
+        this._saldo += valor;           
+    }
+    transferir(valor, conta){
+        conta.cidade = "São Paulo"
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+    }
+}
